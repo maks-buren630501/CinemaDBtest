136749f960f968bd9aae43554e100f3adc38a936
maks-buren630501 <maks.bur2809@gmail.com>
2020-01-20 17:21:11 +0300

    get connection to accdb and first select

--- CinemaDBtest/main.cpp
+++ CinemaDBtest/main.cpp
@@ -1,9 +1,64 @@
+#import <C:\\Program Files\\Common Files\\system\\ado\\msado15.dll> rename( "EOF", "AdoNSEOF" )
+
 #include<iostream>
+#include<sql.h>
+#include<sqlext.h>
+#include <oledb.h>  
+#include <stdio.h>  
+#include <conio.h>  
+#include "icrsint.h"  
+
 
 using namespace std;
 
-void main()
+inline void TESTHR(HRESULT _hr) {
+	if FAILED(_hr)
+		_com_issue_error(_hr);
+}
+
+int main() 
 {
-	cout << 2+2<< endl;
+	_bstr_t bstrConnect = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=F:\\AccesDB\\CinemaDB.accdb;";
+	CoInitialize(NULL);
+	
+		
+		ADODB::_ConnectionPtr pCn("ADODB.Connection");
+		_variant_t vtTableName("authors"), vtCriteria;
+		long ix[1];
+		SAFEARRAY *pSa = NULL;
+
+		pCn->Provider = "sqloledb";
+		HRESULT hr = pCn->Open(bstrConnect, "", "", ADODB::adConnectUnspecified);
+		if (SUCCEEDED(hr)) {
+			cout << ": Successfully connected to database. Data source name:\n  " << pCn->GetConnectionString() << endl;
+		}
+
+		_bstr_t query = "SELECT * FROM Films;";
+		cout << ": SQL query:\n  " << query << endl;
+
+		ADODB::_RecordsetPtr pRS("ADODB.Recordset");
+		hr = pRS->Open(query,
+			_variant_t((IDispatch *)pCn, true),
+			ADODB::adOpenUnspecified,
+			ADODB::adLockUnspecified,
+			ADODB::adCmdText);
+		if (SUCCEEDED(hr)) {
+			cout << ": Retrieve schema info for the given result set: " << endl;
+			ADODB::Fields* pFields = NULL;
+			hr = pRS->get_Fields(&pFields);
+			if (SUCCEEDED(hr) && pFields && pFields->GetCount() > 0) {
+				for (long nIndex = 0; nIndex < pFields->GetCount(); nIndex++) {
+					cout << " | " << _bstr_t(pFields->GetItem(nIndex)->GetName());
+				}
+				cout << endl;
+			}
+			else {
+				cout << ": Error: Number of fields in the result set is 0." << endl;
+			}
+
+		}
+		pCn->Close();
+	
 	system("pause");
+	CoUninitialize();
 }
